7702ffbb75336f042beed75fb9a96bee9cd1f690
Derek Yang <yanghada@gmail.com>
2010-08-12 15:51:09 +0800

    Highlight a named request

--- automation_project/requests.groovy
+++ automation_project/requests.groovy
@@ -1,4 +1,6 @@
-requests = new XmlParser().parse('D:/ws/main/sources/lib/Debug/Record/2170/Record/Recording.xml')
+def highlightName = 'Check in File With Dependencies'
+def requests = new XmlParser().parse('D:/ws/main/sources/lib/Debug/Record/2170/Record/Recording.xml')
 requests.Request.Attributes.Name.each { 
-    println it.text()
+    def name = (it.text() == highlightName) ? "***${it.text()}" : it.text()
+    println name
 }
