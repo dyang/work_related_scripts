f0c947cafb37d95d75cf20174f6ef606266acfa5
Derek Yang <yanghada@gmail.com>
2010-08-23 11:14:32 +0800

    Print request sequence id

--- automation_project/requests.groovy
+++ automation_project/requests.groovy
@@ -1,6 +1,6 @@
 def highlightName = ['DeleteLinks', 'GetUpdatedEntityLinks', 'GetEntityClass']
 def requests = new XmlParser().parse('D:/ws/main/sources/lib/Debug/Record/del-link1/Record/Recording.xml')
-requests.Request.Attributes.Name.each { 
-    def name = highlightName.contains(it.text()) ? "***${it.text()}" : it.text()
-    println name
+requests.Request.Attributes.Name.eachWithIndex { req, index -> 
+    def name = highlightName.contains(req.text()) ? "***${req.text()}" : req.text()
+    println "${index + 1} - ${name}"
 }
