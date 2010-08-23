f9ae18788ed9f563037cc72500bff6636363f091
Derek Yang <yanghada@gmail.com>
2010-08-23 10:53:49 +0800

    Highlight multiple requests

--- automation_project/requests.groovy
+++ automation_project/requests.groovy
@@ -1,6 +1,6 @@
-def highlightName = 'Check in File With Dependencies'
-def requests = new XmlParser().parse('D:/ws/main/sources/lib/Debug/Record/2170/Record/Recording.xml')
+def highlightName = ['DeleteLinks', 'GetUpdatedEntityLinks', 'GetEntityClass']
+def requests = new XmlParser().parse('D:/ws/main/sources/lib/Debug/Record/del-link1/Record/Recording.xml')
 requests.Request.Attributes.Name.each { 
-    def name = (it.text() == highlightName) ? "***${it.text()}" : it.text()
+    def name = highlightName.contains(it.text()) ? "***${it.text()}" : it.text()
     println name
 }
